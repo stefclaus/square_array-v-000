3b3ecbc58facef1a71388667d03a0a1893f3538e
Stef Claus <stef.claus@gmail.com>
2018-09-11 12:26:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 
 def square_array(array)
-  array.each do |number|
-    new_number = number * number 
-    square_array = []
-    return square_array(new_number)
-  end 
+  array.each do |number| 
+    new_number = number * number
+    square_array(new_number)
+  end
 end
 
